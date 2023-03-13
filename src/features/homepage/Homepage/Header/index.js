36cf7f23739f14d0eea70180dacf1c96649ee62b
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-13 13:40:50 +0100

    copy Updated in Header

--- src/features/homepage/Homepage/Header/index.js
+++ src/features/homepage/Homepage/Header/index.js
@@ -10,9 +10,14 @@ const Header = () => (
       <MiniText>this is</MiniText>
       <Title>Patryk Kaniewski</Title>
       <Text>
-        I'm deeply intrested in self improvement. One of my goals is to become
-        front-end developer. Currently learing from youcode Frontend Developer
-        course.
+        Hi! I'm deeply passionate about computer programming as such. I can do
+        it all day and still love it! Currently trying to become Frontend
+        Developer. Checkout my portfolio below, but remember - It's still
+        growing😁 Beyond all of that, I'm amazing, talkative and humble. I know,
+        what work really means and I understand the importance of communication.
+        Right now you have a unique opportunity to hire me. Make sure, you don't
+        miss it.😉
+        PS. I am eager to improve my current skills as well as acquire new ones.
       </Text>
       <ButtonLink
         href={`mailto:${email}`}
